0318c79f860fd2153c9bbce6fb25f3a319da6373
Roman 111 <romanvydysh7@gmail.com>
2024-03-14 15:10:15 +0200

    update

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+
+int findThirdCommaIndex(const std::string& str) {
+    int commaCount = 0;
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] == ',') {
+            commaCount++;
+            if (commaCount == 3) {
+                return i; 
+            }
+        }
+    }
+    return -1; 
+}
+
+
+void replaceCommasWithStars(std::string& str) {
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] == ',') {
+            str.replace(i, 1, "**");
+
+            i++;
+        }
+    }
+}
+
+int main() {
+    std::string str;
+
+    std::cout << "Input: ";
+    std::getline(std::cin, str);
+
+    int commaIndex = findThirdCommaIndex(str);
+    if (commaIndex != -1) {
+        std::cout << "Index: " << commaIndex << std::endl;
+
+        replaceCommasWithStars(str);
+        std::cout << "Width '**': " << str << std::endl;
+    }
+    else {
+        std::cout << "Nothing" << std::endl;
+    }
+
+    return 0;
+}
